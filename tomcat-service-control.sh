b7b2f2f1877f79f7f4d6faa6cb44fee756e1fd73
colynn.liu <colynnliu@foxmail.com>
2015-01-12 14:05:27 +0800

    read CATALINA_HOME/bin/setenv.sh variable,seting jmx.

--- tomcat-service-control.sh
+++ tomcat-service-control.sh
@@ -4,64 +4,77 @@
 #
 # description: script to launch Tomcat on boot
 #
-#mailto: colynn.liu@chinanetcloud.com
+#mailto: colynn.liu
 
-CATALINA_USER=root
-CATALINA_HOME=/usr/local/tomcat7
+CATALINA_USER=tomcat
+CATALINA_HOME=/opt/tomcat1
 CATALINA_BIN="${CATALINA_HOME}/bin"
 CATALINA_CMD="${CATALINA_BIN}/catalina.sh"
-#JAVA_HOME=""
 CATALINA_PID=${CATALINA_HOME}/var/catalina.pid
+CATALINA_OUT=${CATALINA_HOME}/logs/catalina.out
 
+##jmx
+#jmx seting will read ${CATALINA_HOME}/bin/setenv.sh
 
+#define status variable
 RETVAL=0
-prog=tomcat7
 
+start() {
+	if [ -f "$CATALINA_PID" ]; then
+        echo "$CATALINA_PID already exists; process is already running or crashed" 1>&2
+        else
+        echo "Starting Tomcat..."
+        su - $CATALINA_USER -c "${CATALINA_CMD} start"
+        RETVAL=$?
+          if [ $RETVAL -eq 0 ] ; then
+             echo "$CATALINA_HOME tomcat start is ok."
+	     echo "${CATALINA_OUT} log get more infomation."
+          fi
+        fi
+}
 
+stop(){
+        if [ ! -f "$CATALINA_PID" ]; then
+                echo "$CATALINA_PID does not exist; process does not exist or has gone rogue" 1>&2
+        else
+        PID=$(cat $CATALINA_PID)
+        echo "Stopping..."
+        su - $CATALINA_USER -c "kill -9 ${PID}"
+        while [ -x "/proc/${PID}" ]; do
+                echo "waiting for Shutdown..."
+                sleep 1
+        done
+        rm $CATALINA_PID
+        echo "$CATALINA_HOME tomcat stopped is ok."
+        fi
+}
 case "$1" in
-    start)
-        if [ -f "$CATALINA_PID" ]; then
-            echo "$CATALINA_PID already exists; process is already running or crashed" 1>&2
-        else
-            echo "Starting Tomcat..."
-            su - $CATALINA_USER -c "${CATALINA_CMD} start"
-   	    RETVAL=$?
-            if [ $RETVAL -eq 0 ] ; then
-		echo $(ps -ef |grep "$CATALINA_HOME " |grep -v "grep $CATALINA_HOME " |grep "/java" |awk '{print $2}' ) > $CATALINA_PID
-	    	echo "$CATALINA_HOME tomcat start is ok."
-	     fi  
-        fi
-        ;;
-    status)
-        if [ ! -f "$CATALINA_PID" ]; then
-            echo "$CATALINA_PID does not exist; process does not exist or has gone rogue"
-        else
-            echo "pid file exists: ${CATALINA_PID}"
-            PID=$(cat $CATALINA_PID)
-            echo "should be running with pid ${PID}"
-            if [ -x "/proc/${PID}" ]; then
-                echo "process exists in process list at /proc/${PID}"
-            else
-                echo "process does not exist in process list (could not find it at /proc/${PID}; did it die without nuking its own pid file?"
-            fi
-        fi
-        ;;
+   start)
+	start
+	;;
+   status)
+	if [ ! -f "$CATALINA_PID" ]; then
+		echo "$CATALINA_PID does not exist; process does not exist or has gone rogue"
+	else
+		echo "pid file exists: ${CATALINA_PID}"
+	PID=$(cat $CATALINA_PID)
+	echo "${CATALINA_HOME} should be running with pid ${PID}"
+	   if [ -x "/proc/${PID}" ]; then
+		echo "process exists in process list at /proc/${PID}"
+	   else
+		echo "process does not exist in process list (could not find it at /proc/${PID}; did it die without nuking its own pid file?"
+	   fi
+	fi
+	;;
     stop)
-        if [ ! -f "$CATALINA_PID" ]; then
-            echo "$CATALINA_PID does not exist; process does not exist or has gone rogue" 1>&2
-        else
-            PID=$(cat $CATALINA_PID)
-            echo "Stopping..."
-            su - $CATALINA_USER -c "kill  -9 ${PID}"
-            while [ -x "/proc/${PID}" ]; do
-                echo "Waiting for Tomcat to shut down..."
-                sleep 2
-            done
-	    rm $CATALINA_PID
-            echo "Tomcat stopped"
-        fi
-        ;;
+	stop
+	;;
+    restart)
+	stop
+ 	sleep 1
+	start
+	;;
     *)
-        echo $"Usage: $0 {start|stop|status}"
-        ;;
+        echo $"Usage: $0 {start|stop|restart|status}"
+	;;
 esac
