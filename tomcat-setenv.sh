d82cfa28403427c1ade50609b4987c0fd97b9b96
colynn.liu <colynnliu@foxmail.com>
2015-01-14 18:58:17 +0800

    add Djava.rmi.server.hostname

--- tomcat-setenv.sh
+++ tomcat-setenv.sh
@@ -5,6 +5,7 @@ CATALINA_PID=${CATALINA_HOME}/var/catalina.pid
 ##set jmx
 JAVA_OPTS=$JAVA_OPTS" -Dcom.sun.management.jmxremote.port=6789"
 JAVA_OPTS=$JAVA_OPTS" -Dcom.sun.management.jmxremote.host=127.0.0.1"
+JAVA_OPTS=$JAVA_OPTS" -Djava.rmi.server.hostname=127.0.0.1"
 JAVA_OPTS=$JAVA_OPTS" -Dcom.sun.management.jmxremote.ssl=false"
 JAVA_OPTS=$JAVA_OPTS" -Dcom.sun.management.jmxremote.authenticate=true"
 JAVA_OPTS=$JAVA_OPTS" -Dcom.sun.management.jmxremote.access.file=$CATALINA_HOME/conf/jmxremote.access"
